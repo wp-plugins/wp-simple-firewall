392c3552a2fb4832bbc3dfe8694e30000405fac6
paultgoodchild <paultgoodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-05 09:48:34 +0000

    version bump to 2.6.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-simple-firewall/trunk@927027 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icwp-wpsf.php
+++ icwp-wpsf.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Simple Firewall
  * Plugin URI: http://icwp.io/2f
  * Description: A Simple WordPress Firewall
- * Version: 2.6.3
+ * Version: 2.6.4
  * Text Domain: wp-simple-firewall
  * Author: iControlWP
  * Author URI: http://icwp.io/2e
@@ -52,7 +52,7 @@ class ICWP_Wordpress_Simple_Firewall extends ICWP_Feature_Master {
 	 * Should be updated each new release.
 	 * @var string
 	 */
-	const PluginVersion					= '2.6.3';
+	const PluginVersion					= '2.6.4';
 	
 	/**
 	 * @var string
