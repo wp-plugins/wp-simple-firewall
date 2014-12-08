9471004e319e864e7c1c7c0b4b670d90e69da910
paultgoodchild <paultgoodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-08 13:31:13 +0000

    version bump 4.2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-simple-firewall/trunk@1040340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icwp-wpsf.php
+++ icwp-wpsf.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Simple Firewall
  * Plugin URI: http://icwp.io/2f
  * Description: A Simple WordPress Firewall
- * Version: 4.1.4
+ * Version: 4.2.0
  * Text Domain: wp-simple-firewall
  * Author: iControlWP
  * Author URI: http://icwp.io/2e
